48a0c9796f6fc3c185da205da4ed5e365dfe47e2
Axel Bruneaux <gazhrot@gmail.com>
2016-07-22 13:01:00 +0200

    ajout de l'inscription et de la connection fonctionnel pour l'application

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -4,11 +4,38 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('pokeGoChatCtrl', function($scope) {
+.controller('pokeGoChatCtrl', function($scope, $http) {
 
+	$scope.connection = function(login, password) {
+
+		var data = {
+			email: login,
+			password: password
+		};
+
+		$http.post('http://localhost:3000/user/connection', data)
+		.success(function(data) {
+			$scope.connection_error = data.error;
+		}, function(err) {
+			console.log(err.error);
+		})
+	}
 })
    
-.controller('inscriptionCtrl', function($scope) {
+.controller('inscriptionCtrl', function($scope, $http) {
+	
+	$scope.inscription = function(login, password) {
 
+		var data = {
+			email: login,
+			password: password
+		};
+
+		$http.post('http://localhost:3000/user/inscription', data)
+		.success(function(data) {
+			$scope.inscription_error = data.error;
+		}, function(err) {
+			console.log(err.error);
+		})
+	}
 })
-    
